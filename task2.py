ed1a2dfe6ef68901136011c6963e8de349c81b9c
Konstantin <kostpak2002@gmail.com>
2025-03-12 15:30:55 +0200

    123

--- task2.py
+++ task2.py
@@ -8,6 +8,6 @@ def get_numbers_ticket(min: int, max: int, quantity: int):
     numbers = random.sample(range(min, max + 1), quantity)
     return sorted(numbers)
 
-# Приклад використання
+
 lottery_numbers = get_numbers_ticket(1, 49, 6)
 print("Ваші лотерейні числа:", lottery_numbers)
